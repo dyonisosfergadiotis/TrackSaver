1d6ec839d852e466bd8e51421e6008dc8cfe6c86
Dyonisos Fergadiotis <dyonisos.fergadiotis@mail.de>
2026-02-06 12:37:57 +0100

    Initial Commit

---
+++ TrackSaver/TrackSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackSaverApp.swift
+//  TrackSaver
+//
+//  Created by Dyonisos Fergadiotis on 06.02.26.
+//
+
+import SwiftUI
+
+@main
+struct TrackSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
